4f5d6ba56a94484b979c26d22bdf08627a7a4b64
Matthew Fedderly <mdfedderly@mdfedderly.com>
2024-08-06 19:49:16 -0400

    Fix typescript typings

--- types.d.ts
+++ types.d.ts
@@ -1,8 +1,6 @@
 import { FeatureCollection, Feature, GeometryObject } from "geojson";
 
-export type Intersection = [number, number];
-
-export default function sweeplineIntersections(
+export = function sweeplineIntersections(
   geojson: FeatureCollection<GeometryObject> | Feature<GeometryObject>,
   ignoreSelfIntersections: boolean
-): Intersection[];
+): [number, number][];
